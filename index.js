c9ca75b7abf7e6960fa8f03949329c2f3d56d148
ahmedj <ahmedarifusa@gmail.com>
2024-05-09 16:14:18 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    
+    var messages = [];
+
+    
+    for (var i = 0; i < array.length; i++) {
+        
+        var message = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+
+      
+        messages.push(message);
+    }
+
+   
+    return messages;
+}
+
+
+
+function countDown(number) {
+    
+    for (var i = number; i >= 0; i--) {
+        console.log(i); 
+    }
+}
+
+
